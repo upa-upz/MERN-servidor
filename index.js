8b1446cbb21b7304e5af2595e28550edeeafd782
Santiago Fernandez <santy.fernandito@gmail.com>
2020-11-18 23:00:54 -0300

    ajuste para deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -26,6 +26,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 // arrancar la app
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando el puerto ${PORT}`);
 });
